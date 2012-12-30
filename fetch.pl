adc928e2dfb420e9d590dbe34eaa9943db73ca34
Przemysław Wesołek <jest@go.art.pl>
2012-12-30 23:21:50 +0100

    Storing last fetch in a config. Incremental fetching works.

--- fetch.pl
+++ fetch.pl
@@ -5,21 +5,35 @@ use Net::Twitter;
 use JSON;
 use IO::All -utf8;
 
-my ($ddir, $since) = @ARGV;
-die "Usage: $0 data-dir since-id" unless defined $since;
-die "$ddir/$since doesn't exists, exiting" unless -d "$ddir/$since";
+my ($ddir) = @ARGV;
+my $json = JSON->new->ascii;
 
+die "Usage: $0 data-dir" unless defined $ddir;
+my $conf = $json->decode(io("$ddir/config.json")->all);
+my $since = $conf->{max_id} // 0;
 
-my $nt = Net::Twitter->new(traits => [ 'API::Search' ]);
+die "$ddir/$since doesn't exists, exiting" unless $since == 0 or -d "$ddir/$since";
+
+my $nt = Net::Twitter->new(traits => [ qw[ API::REST API::Search RateLimit ] ]);
+$nt->rate_limit_status({ authenticate => 0 });
+printf "%d / %d calls remaining\n", $nt->rate_remaining, $nt->rate_limit;
+
 my $maxid;
 for (my $page = 1; ; ++$page) {
-	my $r = $nt->search({ q => '#rtmstats', rpp => 100, page => $page });
+	my $r = $nt->search({ q => '#rtmstats', rpp => 100, page => $page, since_id => $since });
 	last unless $r and @{$r->{results}};
 	if (!$maxid) {
 		$maxid = $r->{max_id_str};
 		die "Can't find max_id in the results, exiting" unless $maxid;
 		mkdir "$ddir/$maxid" or die "Error creating dir $ddir/$maxid: $!";
 	}
-	to_json($r) > io("$ddir/$maxid/_$page.json");
-	last;
+	$json->encode($r) > io("$ddir/$maxid/_$page.json");
+}
+
+if (! defined $maxid) {
+	say "No new tweets.";
+} else {
+	say "Tweet up to $maxid fetched.";
+	$conf->{max_id} = $maxid;
+	$json->encode($conf) > io "$ddir/config.json";
 }
