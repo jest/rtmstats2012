b5b1cfc7763400675bc2f2c1172fa5d5f03cc2c5
Przemysław Wesołek <jest@go.art.pl>
2013-01-07 17:49:44 +0100

    Experiments with date parsing.

--- parse_json.pl
+++ parse_json.pl
@@ -1,32 +1,39 @@
 use utf8;
 
+use lib 'local/lib/perl5';
 use common::sense;
 use JSON;
 use IO::All -utf8;
+use DateTime::Format::Strptime;
 
 my %qr = (
-	en => qr/I completed ([0-9,]+) tasks with (Remember The Milk|\@rememberthemilk) in 2012(\.?) #rtmstats/i,
-	ja => qr/^2012年に\@rememberthemilkで(\d+)個/,
+	en => qr/I completed ([0-9,]+) tasks with (Remember The Milk|\@rememberthemilk) in 2012/i,
+	ja => qr/^2012年に(?:Remember The Milk|\@rememberthemilk)で(\d+)個/i,
 );
-my $fname = $ARGV[0] // '-';
-my $cnt < io $fname;
-my $answer = from_json $cnt;
+my $strp = DateTime::Format::Strptime->new(pattern => '%a, %d %b %Y %H:%M:%S %z');
+@ARGV = ('-') if !@ARGV;
 
-my $resu = $answer->{results};
-for my $r (@$resu) {
-	my $txt = $r->{text};
-	my ($matched, $num);
-	for my $lang (keys %qr) {
-		next unless $txt =~ $qr{$lang};
-		
-		$matched = $lang;
-		$num = $1;
-		$num =~ s/,//g;
-		last;
-	}
-	if ($matched) {
-		;#say "$matched: $num";
-	} else {
-		say $txt;
+for my $fname (@ARGV) {
+	my $cnt < io $fname;
+	my $answer = from_json $cnt;
+	
+	my $resu = $answer->{results};
+	for my $r (@$resu) {
+		my $txt = $r->{text};
+		my ($matched, $num);
+		for my $lang (keys %qr) {
+			next unless $txt =~ $qr{$lang};
+			
+			$matched = $lang;
+			$num = $1;
+			$num =~ s/,//g;
+			last;
+		}
+		if ($matched) {
+			my $dt = $strp->parse_datetime($r->{created_at});
+			say join ",", $r->{iso_language_code}, $r->{created_at}, $dt->ymd, $dt->hms, $dt->time_zone, $num; 
+		} else {
+#			say $txt;
+		}
 	}
 }
